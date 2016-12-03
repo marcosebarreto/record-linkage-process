d8f230fbb4ad4e773edcad193af01d2291dc7ea8
pedronovaes <pedronovaesmelo@gmail.com>
2016-12-03 15:22:41 -0300

    Adding function to fill matrix with lines of files

--- cuda-c/linkage.c
+++ cuda-c/linkage.c
@@ -7,10 +7,12 @@
 
 #define NCOL 101
 
+void fill_matrix(int matrix[][NCOL], int pos, char *line);
+
 int main(int argc, char const *argv[]) {
     FILE *base_a, *base_b;
     char line[256];
-    int nlines_a = 0, nlines_b = 0, pos_to_insert = 0;
+    int nlines_a = 0, nlines_b = 0, pos_to_insert = 0, i, j;
 
     // opening large base (base_a) and small base (base_b)
     base_a = fopen("base_a10.bloom", "r");
@@ -31,11 +33,19 @@ int main(int argc, char const *argv[]) {
     fgets(line, 255, base_a);
     while (!feof(base_a)) {
         line[strlen(line) - 1] = '\0';
-        // fill_matrix(matrixA, pos_to_insert, line);
-        printf("%s\n", line);
+        fill_matrix(matrixA, pos_to_insert, line);
+        // printf("%s\n", line);
         pos_to_insert++;
         fgets(line, 255, base_a);
     }
+
+    for (i = 0; i < nlines_a; i++) {
+        for (j = 0; j < NCOL; j++) {
+            printf("%d", matrixA[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 
     // ------------------------------ OPERATIONS WITH BASE B ------------------------------ //
     pos_to_insert = 0;
@@ -54,14 +64,46 @@ int main(int argc, char const *argv[]) {
     fgets(line, 255, base_b);
     while (!feof(base_b)) {
         line[strlen(line) - 1] = '\0';
-        // fill_matrix(matrixB, pos_to_insert, line);
-        printf("%s\n", line);
+        fill_matrix(matrixB, pos_to_insert, line);
+        // printf("%s\n", line);
         pos_to_insert++;
         fgets(line, 255, base_b);
     }
+
+    for (i = 0; i < nlines_b; i++) {
+        for (j = 0; j < NCOL; j++) {
+            printf("%d", matrixB[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 
     fclose(base_a);
     fclose(base_b);
 
     return 0;
+}
+
+void fill_matrix(int matrix[][NCOL], int pos, char *line) {
+    int i, j = 0, before_v = 1;
+    char c, id[10];
+
+    for (i = 0; i < strlen(line); i++) {
+        c = line[i];
+        if (before_v == 1) {
+            if (c == ';') {
+                id[i] = '\0';
+                matrix[pos][j] = atoi(id);
+                j++;
+                before_v = 0;
+            }
+            else {
+                id[i] = c;
+            }
+        }
+        else {
+            matrix[pos][j] = line[i] - '0';
+            j++;
+        }
+    }
 }

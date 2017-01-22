92bac8ac50e14e96041497a14f1b3a216ed0b0f3
Clicia dos Santos Pinto <cliciasp1@gmail.com>
2017-01-22 16:01:45 -0300

    Adding input file size as an argument

--- cuda-c/linkage.cu
+++ cuda-c/linkage.cu
@@ -6,8 +6,11 @@
 #include <string.h>
 #include <cuda.h>
 #include <time.h>
+#include <omp.h>
+
 #define NCOL 101
 
+__device__ int contador = 0;
 
 void fill_matrix(int *matrix, int pos, char *line);
 int get_num_of_lines(FILE *fp);
@@ -18,40 +21,50 @@ __device__ float dice(int *bloomA, int *bloomB);
 
 
 int main(int argc, char const *argv[]) {
+    double t1, t2;
+    t1 = omp_get_wtime();
+
     FILE *base_a, *base_b;
+    char file1[30];
+    strcpy(file1, "base_");
+    strcat(file1, argv[2]);
+    strcat(file1, "K.bloom");
+    // strcpy(file1, argv[1]);
+    // strcpy(file2, argv[2]);
+
     int nlines_a = 0, nlines_b = 0;
-    int threads_per_block = atoi(argv[1]); // 512
-    clock_t start, end;
-    double cpu_time_used;
+    // int threads_per_block = 16;
+    int threads_per_block = atoi(argv[1]);
 
     // opening large base (base_a) and small base (base_b)
-    printf("[LOADING DATABASES ... ]\n");
-    base_a = fopen("base_a5k.bloom", "r");
-    base_b = fopen("base_a7k.bloom", "r");
+    // printf("[LOADING DATABASES ... ]\n");
+    base_a = fopen(file1, "r");
+    base_b = fopen("base_1000K.bloom", "r");
 
     // --------------------- OPERATIONS WITH BASE A --------------------- //
     // getting line quantity
-    printf("[GETTING NUMBER LINES FOR BASE A ... ]\n");
+    // printf("[GETTING NUMBER LINES FOR BASE A ... ]\n");
     nlines_a = get_num_of_lines(base_a);
     int *matrixA = (int *)malloc(nlines_a * NCOL * sizeof(int));
 
     // processing base_a to fill matrixA
-    printf("[PROCESSING BASE A ... ]\n");
+    // printf("[PROCESSING BASE A ... ]\n");
     process_file(base_a, matrixA);
-//    print_matrix(matrixA, nlines_a);
+    // print_matrix(matrixA, nlines_a);
 
     // --------------------- OPERATIONS WITH BASE B --------------------- //
     // getting line quantity
-    printf("[GETTING NUMBER LINES FOR BASE B ... ]\n");
+    // printf("[GETTING NUMBER LINES FOR BASE B ... ]\n");
     nlines_b = get_num_of_lines(base_b);
     int *matrixB = (int *)malloc(nlines_b * NCOL * sizeof(int));
 
     // processing base_b to fill matrixB
-    printf("[PROCESSING BASE B ... ]\n");
+    // printf("[PROCESSING BASE B ... ]\n");
     process_file(base_b, matrixB);
-  //  print_matrix(matrixB, nlines_b);
+    // print_matrix(matrixB, nlines_b);
 
-    start = clock();
+    // printf("line_a=%d\tline_b=%d\n", nlines_a, nlines_b);
+
     // ------------------------- CUDA OPERATIONS ------------------------ //
     int *matrixA_d, *matrixB_d;
 
@@ -64,21 +77,29 @@ int main(int argc, char const *argv[]) {
     cudaMemcpy(matrixB_d, matrixB, nlines_b * NCOL * sizeof(int), cudaMemcpyHostToDevice);
 
     // kernel operations
-    printf("[OPERATING AT KERNEL CUDA ... ]\n");
-    dim3 dimGrid = (nlines_a / threads_per_block);
+    // printf("[OPERATING AT KERNEL CUDA ... ]\n");
+    dim3 dimGrid = (int) ceil( (int) nlines_a / (int) threads_per_block);
     dim3 dimBlock = threads_per_block;
     kernel<<<dimGrid, dimBlock>>>(matrixA_d, matrixB_d, nlines_a, nlines_b);
-    cudaDeviceSynchronize();    
+
+    cudaDeviceSynchronize();
+
     // deallocating device memory
     cudaFree(matrixA_d);
     cudaFree(matrixB_d);
 
+    free(matrixA);
+    free(matrixB);
+
+    // close files
     fclose(base_a);
     fclose(base_b);
-   end = clock();
-   cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-//    printf("%d\t%f\n",nlines_a, t2-t1);
-    printf("->Tamanho do problema:\t%d\n->Threads por bloco:\t%d \n->Blocos:\t%d \n->Tempo:\t%f\n",nlines_a, threads_per_block ,nlines_a/threads_per_block, cpu_time_used);
+
+    t2 = omp_get_wtime();
+
+    // printf("%d\t%d\t%d\t%f\n", nlines_a, threads_per_block, nlines_a/threads_per_block, t2-t1);
+    int tam_prob = atoi(argv[2]);
+    printf("%d\t%f\n", (tam_prob * 1000), (t2-t1));
 
     return 0;
 }
@@ -88,10 +109,13 @@ int main(int argc, char const *argv[]) {
 int get_num_of_lines(FILE *fp) {
     int lines = 0;
     char line[256];
-    if(!fgets(line, 255, fp)) printf("fp = NULL");
+    if(!fgets(line, 255, fp))
+        printf("fp = NULL");
+
     while (!feof(fp)) {
         lines++;
-	if(!fgets(line, 255, fp)) break;
+        if(!fgets(line, 255, fp))
+            break;
     }
 
     return lines;
@@ -106,13 +130,15 @@ void process_file(FILE *fp, int *matrix) {
     rewind(fp);
 
     // getting line by line to insert into matrix
-    if(!fgets(line, 255, fp)) printf("fp = NULL");
+    if(!fgets(line, 255, fp))
+        printf("fp = NULL");
     while (!feof(fp)) {
         line[strlen(line) - 1] = '\0';
         fill_matrix(matrix, pos_to_insert, line);
 
         pos_to_insert++;
-	if(!fgets(line, 255, fp)) break;
+        if(!fgets(line, 255, fp))
+            break;
     }
 }
 
@@ -158,8 +184,8 @@ void print_matrix(int *matrix, int nlines) {
 
 __global__ void kernel(int *matrixA, int *matrixB, int nlines_a, int nlines_b){
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-//    printf("I = %d - blockID.x= %d e blockId.y = %d -- blockDim.x = %d e blockDim.y = %d -- threadIdx.x = %d e threadIdx.y = %d\n", i, blockIdx.x, blockIdx.y, blockDim.x, blockDim.y, threadIdx.x, threadIdx.y);  
-  // int j = blockIdx.y * blockDim.y + threadIdx.y;
+    // printf("I = %d - blockID.x= %d e blockId.y = %d -- blockDim.x = %d e blockDim.y = %d -- threadIdx.x = %d e threadIdx.y = %d\n", i, blockIdx.x, blockIdx.y, blockDim.x, blockDim.y, threadIdx.x, threadIdx.y);
+    // int j = blockIdx.y * blockDim.y + threadIdx.y;
 
     int bloomA[100], bloomB[100];
 
@@ -176,17 +202,15 @@ __global__ void kernel(int *matrixA, int *matrixB, int nlines_a, int nlines_b){
             for (int l = 1; l < 101; l++) {
                 bloomB[l - 1] = matrixB[k * NCOL + l];
             }
-              dice(bloomA, bloomB);
+            dice(bloomA, bloomB);
         }
     }
+
+    // printf("num de comparacoes para thread %d: %d\n", i, contador);
 }
 
 
 // device function to calculate dice coefficient using bloom filter
-//__device__ float dice(int *bloomA, int *bloomB) {
-//    printf("teste\n");
-//}
-
 __device__ float dice(int *bloomA, int *bloomB) {
     float a = 0, b = 0, h = 0;
     int i;
@@ -202,8 +226,8 @@ __device__ float dice(int *bloomA, int *bloomB) {
         }
     }
     float dice = ((h * 2.0) / (a + b)) * 10000;
+    // printf("%.1f\n", dice);
+    // contador++;
+
     return dice;
-
-    //printf("%.1f\n", dice);
-
 }
